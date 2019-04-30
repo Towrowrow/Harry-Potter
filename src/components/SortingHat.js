2c72c0b51ce0a918cf1c730b004ad3689d183239
Tilmant Rémi <towtowrow@gmail.com>
2019-04-30 16:25:18 +0200

    t

--- src/components/SortingHat.js
+++ src/components/SortingHat.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import getRandomHouse from '../functions/randomizer.js'
+import SweetAlert from 'sweetalert2-react';
 
 class SortingHat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       YourHouse: "",
-      phrase: ""
+      phrase: "",
+      show: "false",
+      house: "",
     }
   }
 
-  sorting = () => alert("Felicitation tu es admis à " + getRandomHouse());
+  sorting = () => {
+    return (
+      <SweetAlert text="Felicitation tu es admis à " />
+
+    )
+  }
 
 
   render() {
@@ -20,6 +28,14 @@ class SortingHat extends React.Component {
           onClick={this.sorting}
           src="https://images.pottermore.com/bxd3o8b291gf/4KonItodO0UE44EYAySg46/24abcc9e548144d0c1be6e401d88281e/SortingHat_WB_F1_SortingHatAlone_Promo_100615_Land.jpg?w=1330&q=85"
           alt="Sorting hat"
+        />
+
+        <button onClick={() => this.setState({ show: true })}>Découvre ta maison</button>
+        <SweetAlert
+          show={this.state.show}
+          title="Félicitation te voila admis à  ..."
+          text={getRandomHouse()}
+          onConfirm={() => this.setState({ show: false })}
         />
       </div >
     );
