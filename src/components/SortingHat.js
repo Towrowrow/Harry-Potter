7570a2078d4f1e60a714e68831d55d480d863d69
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-30 16:28:02 +0200

    sorting OK

--- src/components/SortingHat.js
+++ src/components/SortingHat.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import getRandomHouse from '../functions/randomizer.js'
+import SweetAlert from 'sweetalert2-react';
 
 class SortingHat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       YourHouse: "",
-      phrase: ""
+      phrase: "",
+      show: false,
     }
   }
 
-  sorting = () => alert("Felicitation tu es admis à " + getRandomHouse());
-
+  sorting = () =>
+    this.setState({
+      show: true,
+      YourHouse: getRandomHouse(),
+    })
 
   render() {
     return (
       <div>
         <img
-          onClick={this.onClickHat}
+          className="sortingHat"
+          onClick={this.sorting}
           src="https://images.pottermore.com/bxd3o8b291gf/4KonItodO0UE44EYAySg46/24abcc9e548144d0c1be6e401d88281e/SortingHat_WB_F1_SortingHatAlone_Promo_100615_Land.jpg?w=1330&q=85"
           alt="Sorting hat"
+        />
+        <SweetAlert
+          show={this.state.show}
+          title="Le choixpeau magique a choisi !"
+          text={`tu es un ${getRandomHouse()}`}
+          onConfirm={() => this.setState({ show: false })}
         />
       </div >
     );
