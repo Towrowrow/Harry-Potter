60323dba3f8e2ee1a26c2d4d1e21449ad7f08d22
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-30 14:07:38 +0200

    debut harry potter api

---
+++ src/components/CharactersList.js
@@ -0,0 +1,69 @@
+import React from "react";
+import axios from "axios";
+// import SweetAlert from 'sweetalert2-react';
+import Capitzalize from '../functions/Capitalize';
+
+class CharactersList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      charactersList: "",
+      show: false,
+    }
+
+  }
+  componentDidMount() {
+    this.getCharactersLists();
+  }
+
+  getCharactersLists() {
+
+    axios.get(`http://hp-api.herokuapp.com/api/characters`)
+      .then(response => (response.data))
+      .then(data => this.setState({ charactersList: data }))
+  };
+
+  render() {
+    return (
+      <div>
+
+        {this.state.charactersList
+          &&
+          this.state.charactersList.map((character, i) => {
+            return (
+              <div key={i}>
+                <img src={character.image} alt={character.name} />
+                <p>Nom :{character.name}</p>
+                <p>Espèce : {character.species}</p>
+                <p>Maison : {character.house}</p>
+                <p>Genre : {character.gender}</p>
+                <p>Ascendance : {character.ancestry}</p>
+                <p>Baguette magique : {Capitzalize(character.wand.wood)}, {Capitzalize(character.wand.core)}, {Number(character.wand.length) * 2.54} cm</p>
+                <p>Patronus : {Capitzalize(character.patronus)}</p>
+
+                {/* <button onClick={() => this.setState({ show: true })}>See the starship</button>
+                <SweetAlert
+                  show={this.state.show}
+                  title="STARSHIPS"
+                  text={character.starships}
+                  onConfirm={() => this.setState({ show: false })}
+                /> */}
+
+
+                <br />
+              </div>
+            )
+          }
+          )
+
+        }
+
+
+      </div>
+
+    );
+
+  }
+
+}
+export default CharactersList;
